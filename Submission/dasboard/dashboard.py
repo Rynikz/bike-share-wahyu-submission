38e22a545b46a609b5479c8cdc31e23c8233d38b
Rynikz <55326474+Rynikz@users.noreply.github.com>
2025-03-10 18:26:13 +0700

    Update dashboard.py

--- Submission/dasboard/dashboard.py
+++ Submission/dasboard/dashboard.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import streamlit as st
 
 # Library---------------------------------------------------
-day_df = pd.read_csv("main_data.csv")
+day_df = pd.read_csv("/mount/src/submission-bike-share/Submission/dasboard/main_data.csv") 
 day_df["dteday"] = pd.to_datetime(day_df["dteday"])
 
 mnth_name = {
